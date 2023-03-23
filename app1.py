13ba7f380584e9e82ff5c05f692834b99b4f0cd5
vtv2186 <vishnu.vithala@gmail.com>
2023-03-23 14:01:02 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -42,6 +42,6 @@ class VideoProcessor:
 ctx=webrtc_streamer(key="example",
                 video_processor_factory=VideoProcessor,
                 rtc_configuration={  # Add this line
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+        "iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]
     }
     )
