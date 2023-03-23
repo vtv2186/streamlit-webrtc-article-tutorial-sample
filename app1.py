351ec4767726e8bd7654334ef4f51866bd4e4105
vtv2186 <vishnu.vithala@gmail.com>
2023-03-23 15:50:22 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -42,6 +42,7 @@ class VideoProcessor:
 ctx=webrtc_streamer(key="example",
                 video_processor_factory=VideoProcessor,
                 rtc_configuration={  # Add this line
-        "iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]
+      # "iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]
+       "iceServers": [{   "urls": [ "stun:ws-turn1.xirsys.com" ]}, {   "username": "_gOvGuKm6kPUXW7I78axfsf8e7hlY2VaJziOfzYjFnnEZqUb50vvQhQQzevloqKTAAAAAGQc1ox2aXNobnV0ZWph",   "credential": "6b2aa7c4-c9cc-11ed-b509-0242ac140004",   "urls": [       "turn:ws-turn1.xirsys.com:80?transport=udp",       "turn:ws-turn1.xirsys.com:3478?transport=udp",       "turn:ws-turn1.xirsys.com:80?transport=tcp",       "turn:ws-turn1.xirsys.com:3478?transport=tcp",       "turns:ws-turn1.xirsys.com:443?transport=tcp",       "turns:ws-turn1.xirsys.com:5349?transport=tcp"   ]}]
     }
     )
